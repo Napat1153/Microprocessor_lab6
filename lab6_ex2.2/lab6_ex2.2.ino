425dd140498a2020ae0142d896283dc85432c013
Napat1153 <60214342+Napat1153@users.noreply.github.com>
2020-02-23 22:27:15 +0700

    Update lab6_ex2.2.ino

--- lab6_ex2.2/lab6_ex2.2.ino
+++ lab6_ex2.2/lab6_ex2.2.ino
@@ -10,7 +10,7 @@ void setup()
 
 void loop()
   {
-    if(Sec > 10)
+    if(Sec > 120)
     {
       digitalWrite(pinLed,LOW);
       delay(500);
@@ -24,7 +24,7 @@ void loop()
       Serial.println(Sec);
       Sec = Sec+2;
     }
-    if(Sec == 10)
+    if(Sec == 120)
     {
       Serial.print("STOP");
     }
